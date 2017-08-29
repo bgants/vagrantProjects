92d669e84cbf0bf5f9763dbe6d6cddc51ace6616
bobgants@gmail.com <bobgants@gmail.com>
2017-08-29 16:48:05 -0400

    modified.

--- pythonDev/Vagrant-setup/bootstrap.sh
+++ pythonDev/Vagrant-setup/bootstrap.sh
@@ -1,12 +1,18 @@
 #!/bin/sh -e
 
+#Bleeding edge ubuntu has Python 3 if you want the Anaconda stuff then comment out below and comment Virtual env/pip below
 #Ok this is ther version of anaconda we want to install
-anaconda=Anaconda3-4.0.0-Linux-x86_64.sh
+#anaconda=Anaconda3-4.0.0-Linux-x86_64.sh
 
 #Ok get your some anaconda then
-wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/$anaconda
+#wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/$anaconda
 
 export DEBIAN_FRONTEND=noninteractive
+
+#Comment out this block and uncomment the anaconda and wget to install just Anaconda rel.
+apt-get install python3-setuptools -y
+easy_install3 pip       # will be a Python3 pip
+pip install virtualenv  # will be py3
 
 PROVISIONED_ON=/etc/vm_provision_on_timestamp
 
@@ -16,16 +22,4 @@ then
   echo "To run system updates manually login via 'vagrant ssh' and run 'apt-get update && apt-get upgrade'"
   echo "" 
   exit
-fi
-
-if [ -s $anaconda ]
-then
-  chmod +x $anaconda
-  ./$anaconda -b -p /opt/anaconda
-  cat >> /home/vagrant/.bashrc << END
-  # For anaconda
-  PATH=/opt/anaconda/bin:$PATH
-END
-else
-  echo "ERROR: Anaconda installer is not found"
 fi
